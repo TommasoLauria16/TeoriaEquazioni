a37870bec303441f6c4a5a6ff93adbc8ef85cc76
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-15 09:06:09 +0100

    Add project files.

---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    {
+        public static bool IsDeterminated(double a)
+        {
+            if (a != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+            
+        }
+    }
+}
