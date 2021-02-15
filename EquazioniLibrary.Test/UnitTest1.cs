39c2c37db675cc0dfff67a80abe31adc73fda23d
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-15 09:13:18 +0100

    aggiornamento

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -1,13 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using EquazioniLibrary;
 namespace EquazioniLibrary.Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEquazionePrimoGradoDeterminata()
         {
+            double a = 6;
+            bool valoreAspettato = true;
+            bool ValoreRitorno = Equazioni.IsDeterminated(a);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
         }
     }
 }
