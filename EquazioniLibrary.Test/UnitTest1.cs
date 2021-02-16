5d953b40476cff814be9e05b9f88148bddbaa528
tommaso lauria <tommaso.lauria9@gmail.com>
2021-02-16 13:05:54 +0100

    aggiornamento

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -11,7 +11,112 @@ namespace EquazioniLibrary.Test
             double a = 6;
             bool valoreAspettato = true;
             bool ValoreRitorno = Equazioni.IsDeterminated(a);
-            Assert.AreEqual(valoreAspettato,ValoreRitorno);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazionePrimoGradoDeterminata1()
+        {
+            double a = 0;
+            bool valoreAspettato = false;
+            bool ValoreRitorno = Equazioni.IsDeterminated(a);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneDiPrimoGradoImpossibile()
+        {
+            double a = 6, b = 4;
+            bool valoreAspettato = true;
+            bool ValoreRitorno = Equazioni.isIncosisted(a, b);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void TestEquazioneDiPrimoGradoImpossibile1()
+        {
+            double a = 0, b = 4;
+            bool valoreAspettato = false;
+            bool ValoreRitorno = Equazioni.isIncosisted(a, b);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void TestEquazioneDiPrimoGradoImpossibile2()
+        {
+            double a = 0, b = 0;
+            bool valoreAspettato = false;
+            bool ValoreRitorno = Equazioni.isIncosisted(a, b);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void TestEquazioneDiPrimoGradoImpossibile3()
+        {
+            double a = 6, b = 0;
+            bool valoreAspettato = false;
+            bool ValoreRitorno = Equazioni.isIncosisted(a, b);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoGrado()
+        {
+            double a = 6;
+            bool valoreAspettato = true;
+            bool ValoreRitorno = Equazioni.isDegree2(a);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoGrado1()
+        {
+            double a = 0;
+            bool valoreAspettato = false;
+            bool ValoreRitorno = Equazioni.isDegree2(a);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoDelta()
+        {
+            double a = 3, b = 4, c = 7;
+
+            double valoreAspettato = -68;
+            double ValoreRitorno = Equazioni.Delta(a,b,c);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoDelta2()
+        {
+            double a = 4, b = 7, c =-7;
+
+            double valoreAspettato = 161;
+            double ValoreRitorno = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoDeltaNullo()
+        {
+            double a = 4, b = -4, c = 1;
+
+            double valoreAspettato = 0;
+            double ValoreRitorno = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoCompleta()
+        {
+            double a = 1, b = 2, c = 1;
+
+            string valoreAspettato = $"{-1}";
+            string ValoreRitorno = Equazioni.Eq2Grado(a, b, c);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
+        }
+        [TestMethod]
+        public void TestEquazioneSecondoCompleta2()
+        {
+            double a = 2, b = 5, c = 2;
+
+            string valoreAspettato = $"{-0.5}-{-2}";
+            string ValoreRitorno = Equazioni.Eq2Grado(a, b, c);
+            Assert.AreEqual(valoreAspettato, ValoreRitorno);
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -14,5 +14,43 @@ namespace EquazioniLibrary
                 return false;
 
         }
+        public static bool isIncosisted(double a, double b)
+        {
+            if (a != 0)
+            {
+                if (b != 0)
+                    return true;
+            }
+            return false;
+        }
+        public static bool isDegree2(double a)
+        {
+            return a != 0;
+        }
+        public static double Delta(double a, double b, double c)
+        {
+            return b * b - (4 * a * c);
+        }
+        public static string Eq2Grado(double a, double b, double c)
+        {
+            double x1, x2;
+            double delta =Delta(a,b,c), radiceDelta;
+            if (delta > 0)
+            {
+                radiceDelta = Math.Sqrt(delta);
+                x1 = (-b + radiceDelta) / (2 * a);
+                x2 = (-b - radiceDelta) / (2 * a);
+                return $"{x1}-{x2}";
+            }else if (delta == 0)
+            {
+                return $"{-b / 2 * a}";
+            }
+            else
+            {
+                return "non ha punti di intersezione";
+            }
+            
+        }
+
     }
 }
